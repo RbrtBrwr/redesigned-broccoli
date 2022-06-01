c9559cbf3fb4573db6248b974604aa7767c172a1
alejandro.pons@correo.unimet.edu.ve <alejandro.pons@correo.unimet.edu.ve>
2022-06-01 11:58:06 -0400

    Pegue todo lo de json

--- PlantaSony/src/Classes/jsonReaderWriter.java
+++ PlantaSony/src/Classes/jsonReaderWriter.java
@@ -4,10 +4,91 @@
  */
 package Classes;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  *
  * @author ponsa
  */
 public class jsonReaderWriter {
-    
+    public static void write(){
+            //First Employee
+            int i = 1;
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava");
+         
+        JSONObject employeeObject = new JSONObject(); 
+        employeeObject.put("employee", employeeDetails);
+         
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+         
+        JSONObject employeeObject2 = new JSONObject(); 
+        employeeObject2.put("employee", employeeDetails2);
+         
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+        //Write JSON file
+        try (FileWriter file = new FileWriter("corridas.json", true)) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(employeeList.toJSONString()); 
+            file.write(System.getProperty("line.separator"));
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+        
+        public static void read() throws org.json.simple.parser.ParseException{
+            //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("employees1.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+             
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+        
+        private static void parseEmployeeObject(JSONObject employee) 
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+         
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");    
+        System.out.println(firstName);
+         
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");  
+        System.out.println(lastName);
+         
+        //Get employee website name
+        String website = (String) employeeObject.get("website");    
+        System.out.println(website);
+    }
 }
